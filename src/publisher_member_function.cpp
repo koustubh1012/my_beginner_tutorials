18dd662732e995dfb31b75eb2c230522476ff197
koustubh1012 <koustubh1012@gmail.com>
2024-11-04 18:37:48 -0500

    Task #4 Modified published stirng
    In this commit, I have modified the string published by the talker
    node

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -1,17 +1,13 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+/**
+ * @file publisher_member_function.cpp
+ * @author koustubh (koustubh@umd.edu)
+ * @brief This file contains a simple ROS2 cpp publisher node
+ * @version 0.1
+ * @date 2024-11-04
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -40,7 +36,7 @@ private:
   void timer_callback()
   {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
+    message.data = "Hello, from MAGE student " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
