fbad51a1ea151dfc85b9e6f78c824f3323b154c4
koustubh1012 <koustubh1012@gmail.com>
2024-11-05 17:55:17 -0500

    Task #9 Added Service to change string in publisher node
    In this commit, I have made changes to cpp file to include the service to change string in publisher node

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -2,7 +2,7 @@
  * @file publisher_member_function.cpp
  * @author koustubh (koustubh@umd.edu)
  * @brief This file contains a simple ROS2 cpp publisher node
- * @version 0.1
+ * @version 0.2
  * @date 2024-11-04
  *
  * @copyright Copyright (c) 2024
@@ -11,15 +11,15 @@
 #include <chrono>
 #include <functional>
 #include <memory>
+#include <rclcpp/logger.hpp>
+#include <rclcpp/logging.hpp>
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/string.hpp" 
+#include "beginner_tutorials/srv/modify_string.hpp"
 
 using namespace std::chrono_literals;
-
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
 
 class MinimalPublisher : public rclcpp::Node {
  public:
@@ -27,18 +27,29 @@ class MinimalPublisher : public rclcpp::Node {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
         500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    service_ = this->create_service<beginner_tutorials::srv::ModifyString>(
+        "modify_string", std::bind(&MinimalPublisher::changeString, this, std::placeholders::_1, std::placeholders::_2));
   }
 
  private:
   void timer_callback() {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, from MAGE student " + std::to_string(count_++);
+    message.data = "Hello, from " + service_message + " : " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
+  }
+  void changeString(const std::shared_ptr<beginner_tutorials::srv::ModifyString::Request> request, 
+                    const std::shared_ptr<beginner_tutorials::srv::ModifyString::Response> response){
+    service_message = request->input;
+    response->output = request->input;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: [%s]",request->input.c_str());                                         // CHANGE
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%s]", response->output.c_str());
   }
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
+  std::string service_message = "Koustubh";
+  rclcpp::Service<beginner_tutorials::srv::ModifyString>::SharedPtr service_;
 };
 
 int main(int argc, char* argv[]) {
