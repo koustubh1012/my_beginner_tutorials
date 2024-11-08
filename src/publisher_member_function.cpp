70bd173466e4c76255c85c7c7b8920fec98339eb
koustubh1012 <koustubh1012@gmail.com>
2024-11-08 12:48:20 -0500

    Task #16 Performed cpp-lint, clag-tidy and cland-format
    In this commit, I have performed Performed cpp-lint, clag-tidy and cland-format
    on all the *.cpp files

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -3,7 +3,7 @@
  * @author koustubh (koustubh@umd.edu)
  * @brief This file contains a simple ROS2 cpp publisher node
  * @version 2.0
- * @date 2024-11-04
+ * @date 2024-11-08
  *
  * @copyright Copyright (c) 2024
  *
@@ -17,9 +17,9 @@
 #include <stdexcept>
 #include <string>
 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp" 
 #include "beginner_tutorials/srv/modify_string.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 // using std::placeholders::_1;
 using STRING = std_msgs::msg::String;
@@ -28,20 +28,33 @@ using TIMER = rclcpp::TimerBase::SharedPtr;
 using PARAMETER_EVENT = std::shared_ptr<rclcpp::ParameterEventHandler>;
 using PARAMETER_HANDLE = std::shared_ptr<rclcpp::ParameterCallbackHandle>;
 
+/**
+ * @brief Class MinimalPublisher to define a node that publishes a string to a
+ * topic
+ *
+ */
 class MinimalPublisher : public rclcpp::Node {
  public:
+  /**
+   * @brief Construct a new Minimal Publisher object
+   * @param node_name Name of the publisher node
+   * @param freq ROS parameter to set the frequency of publisher
+   */
   MinimalPublisher() : Node("minimal_publisher"), count_(0) {
     this->get_logger().set_level(rclcpp::Logger::Level::Debug);
-    // Set up the "freq" parameter with a default value of 2 Hz and a description
+    // Set up the "freq" parameter with a default value of 2 Hz and a
+    // description
     auto param_desc = rcl_interfaces::msg::ParameterDescriptor();
     param_desc.description = "Set callback frequency.";
     this->declare_parameter("freq", 2.0, param_desc);
-    
+
     // Retrieve the initial frequency and set up the parameter event handler
     auto freq = this->get_parameter("freq").as_double();
     m_param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
-    auto paramCallbackPtr = std::bind(&MinimalPublisher::param_callback, this, std::placeholders::_1);
-    m_param_handle_ = m_param_subscriber_->add_parameter_callback("freq", paramCallbackPtr);
+    auto paramCallbackPtr = std::bind(&MinimalPublisher::param_callback, this,
+                                      std::placeholders::_1);
+    m_param_handle_ =
+        m_param_subscriber_->add_parameter_callback("freq", paramCallbackPtr);
 
     // Set up the publisher and timer with the initial frequency
     publisher_ = this->create_publisher<STRING>("topic", 10);
@@ -51,42 +64,74 @@ class MinimalPublisher : public rclcpp::Node {
 
     // Service to change string content dynamically
     service_ = this->create_service<beginner_tutorials::srv::ModifyString>(
-        "modify_string", std::bind(&MinimalPublisher::changeString, this, std::placeholders::_1, std::placeholders::_2));
+        "modify_string",
+        std::bind(&MinimalPublisher::changeString, this, std::placeholders::_1,
+                  std::placeholders::_2));
   }
 
  private:
-  // Timer callback to publish messages at the specified frequency
+  /**
+   * @brief Timer callback to publish messages at the specified frequency
+   *
+   */
   void timer_callback() {
     auto message = STRING();
-    message.data = "Hello, from " + service_message + " : " + std::to_string(count_++);
-    RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: ' " << message.data.c_str() << "'");
-    if (count_%10 == 0){
-      RCLCPP_DEBUG_STREAM(this->get_logger(), "Current publisher rate: " << this->get_parameter("freq").as_double());
+    message.data =
+        "Hello, from " + service_message + " : " + std::to_string(count_++);
+    RCLCPP_INFO_STREAM(this->get_logger(),
+                       "Publishing: ' " << message.data.c_str() << "'");
+    // Print publisher rate after every 10 iterations to debug
+    if (count_ % 10 == 0) {
+      RCLCPP_DEBUG_STREAM(this->get_logger(),
+                          "Current publisher rate: "
+                              << this->get_parameter("freq").as_double());
     }
     publisher_->publish(message);
   }
 
-  // Service callback to change the string content
-  void changeString(const std::shared_ptr<beginner_tutorials::srv::ModifyString::Request> request, 
-                    const std::shared_ptr<beginner_tutorials::srv::ModifyString::Response> response) {
+  /**
+   * @brief Service callback to change the string content
+   *
+   * @param request request sent to server
+   * @param response response sent by server
+   */
+  void changeString(
+      const std::shared_ptr<beginner_tutorials::srv::ModifyString::Request>
+          request,
+      const std::shared_ptr<beginner_tutorials::srv::ModifyString::Response>
+          response) {
     service_message = request->input;
     response->output = request->input;
-    RCLCPP_INFO_STREAM(this->get_logger(), "Incoming request, Change string to: [" << request->input.c_str() << "]");
-    RCLCPP_INFO_STREAM(this->get_logger(), "Sending back response: [" << response->output.c_str() << "]");
+    RCLCPP_INFO_STREAM(this->get_logger(),
+                       "Incoming request, Change string to: ["
+                           << request->input.c_str() << "]");
+    RCLCPP_INFO_STREAM(this->get_logger(), "Sending back response: ["
+                                               << response->output.c_str()
+                                               << "]");
   }
 
-  // Callback to handle frequency parameter updates dynamically
-  void param_callback(const rclcpp::Parameter & param) {
-    RCLCPP_WARN_STREAM(this->get_logger(), "cb: Updated Frequency To: " << param.as_double());
+  /**
+   * @brief Callback to handle frequency parameter updates dynamically
+   *
+   * @param param frequency change parameter
+   */
+  void param_callback(const rclcpp::Parameter& param) {
+    RCLCPP_WARN_STREAM(this->get_logger(),
+                       "cb: Updated Frequency To: " << param.as_double());
 
-    if (param.as_double() > 1000.0){
-      RCLCPP_WARN_STREAM(this->get_logger(), "Frequency too high, might not able to publish at same rate");
+    if (param.as_double() > 1000.0) {
+      RCLCPP_WARN_STREAM(
+          this->get_logger(),
+          "Frequency too high, might not able to publish at same rate");
     }
 
     // Update the timer with the new frequency
-    auto period = std::chrono::milliseconds(static_cast<int>(1000 / param.as_double()));
-    timer_->cancel(); // Stop the current timer
-    timer_ = this->create_wall_timer(period, std::bind(&MinimalPublisher::timer_callback, this)); // Restart timer with new rate
+    auto period =
+        std::chrono::milliseconds(static_cast<int>(1000 / param.as_double()));
+    timer_->cancel();  // Stop the current timer
+    timer_ = this->create_wall_timer(
+        period, std::bind(&MinimalPublisher::timer_callback,
+                          this));  // Restart timer with new rate
   }
 
   rclcpp::TimerBase::SharedPtr timer_;
@@ -104,4 +149,3 @@ int main(int argc, char* argv[]) {
   rclcpp::shutdown();
   return 0;
 }
-
--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -3,7 +3,7 @@
  * @author Koustubh (koustubh@umd.edu)
  * @brief A simple cpp ROS2 subscriber
  * @version 2.0
- * @date 2024-11-04
+ * @date 2024-11-08
  *
  * @copyright Copyright (c) 2024
  *
@@ -18,24 +18,47 @@
 
 using std::placeholders::_1;
 
+/**
+ * @brief Class MinimalSubscriber to define a node that publishes a string to a
+ * topic
+ *
+ */
 class MinimalSubscriber : public rclcpp::Node {
  public:
+  /**
+   * @brief Construct a new Minimal Subscriber object
+   * @param node_name Name of the subscriber node
+   */
   MinimalSubscriber() : Node("minimal_subscriber") {
+    // create subscription handle
     subscription_ = this->create_subscription<std_msgs::msg::String>(
         "/topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
     message_received_ = false;
-    timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&MinimalSubscriber::check_for_messages, this));
+    // create wall timer to check regularly for messages
+    timer_ = this->create_wall_timer(
+        std::chrono::seconds(1),
+        std::bind(&MinimalSubscriber::check_for_messages, this));
   }
 
  private:
+  /**
+   * @brief Topic callback to handle messages
+   *
+   * @param msg Message received from topic
+   */
   void topic_callback(const std_msgs::msg::String& msg) {
     RCLCPP_INFO_STREAM(this->get_logger(), "I heard: " << msg.data.c_str());
     message_received_ = true;
   }
 
-  void check_for_messages(){
-    if(!message_received_){
-      RCLCPP_ERROR_STREAM(this->get_logger(), "Topic 'topic' not published yet..");
+  /**
+   * @brief Function to check if the publisher has published yet
+   *
+   */
+  void check_for_messages() {
+    if (!message_received_) {
+      RCLCPP_ERROR_STREAM(this->get_logger(),
+                          "Topic 'topic' not published yet..");
     }
   }
 
