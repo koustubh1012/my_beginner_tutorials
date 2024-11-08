d537228589cc5ccbe37361c82adde33d6d98cd5b
koustubh1012 <koustubh1012@gmail.com>
2024-11-08 08:48:09 -0500

    Task #13 Added dynamic frequency setting
    In this commit, I have added function to dynamically change the frequency
    of the node once it starts running

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -8,6 +8,7 @@
  * @copyright Copyright (c) 2024
  *
  */
+
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -19,39 +20,74 @@
 #include "std_msgs/msg/string.hpp" 
 #include "beginner_tutorials/srv/modify_string.hpp"
 
-using namespace std::chrono_literals;
+// using std::placeholders::_1;
+using STRING = std_msgs::msg::String;
+using PUBLISHER = rclcpp::Publisher<STRING>::SharedPtr;
+using TIMER = rclcpp::TimerBase::SharedPtr;
+using PARAMETER_EVENT = std::shared_ptr<rclcpp::ParameterEventHandler>;
+using PARAMETER_HANDLE = std::shared_ptr<rclcpp::ParameterCallbackHandle>;
 
 class MinimalPublisher : public rclcpp::Node {
  public:
   MinimalPublisher() : Node("minimal_publisher"), count_(0) {
-    this->declare_parameter("publisher_rate", 500);
-    publisher_rate_ = std::chrono::milliseconds(this->get_parameter("publisher_rate").as_int());
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(publisher_rate_, std::bind(&MinimalPublisher::timer_callback, this));
+    // Set up the "freq" parameter with a default value of 2 Hz and a description
+    auto param_desc = rcl_interfaces::msg::ParameterDescriptor();
+    param_desc.description = "Set callback frequency.";
+    this->declare_parameter("freq", 2.0, param_desc);
+    
+    // Retrieve the initial frequency and set up the parameter event handler
+    auto freq = this->get_parameter("freq").as_double();
+    m_param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
+    auto paramCallbackPtr = std::bind(&MinimalPublisher::param_callback, this, std::placeholders::_1);
+    m_param_handle_ = m_param_subscriber_->add_parameter_callback("freq", paramCallbackPtr);
+
+    // Set up the publisher and timer with the initial frequency
+    publisher_ = this->create_publisher<STRING>("topic", 10);
+    auto period = std::chrono::milliseconds(static_cast<int>(1000 / freq));
+    auto topicCallbackPtr = std::bind(&MinimalPublisher::timer_callback, this);
+    timer_ = this->create_wall_timer(period, topicCallbackPtr);
+
+    // Service to change string content dynamically
     service_ = this->create_service<beginner_tutorials::srv::ModifyString>(
         "modify_string", std::bind(&MinimalPublisher::changeString, this, std::placeholders::_1, std::placeholders::_2));
   }
 
  private:
+  // Timer callback to publish messages at the specified frequency
   void timer_callback() {
-    auto message = std_msgs::msg::String();
+    auto message = STRING();
     message.data = "Hello, from " + service_message + " : " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
+
+  // Service callback to change the string content
   void changeString(const std::shared_ptr<beginner_tutorials::srv::ModifyString::Request> request, 
-                    const std::shared_ptr<beginner_tutorials::srv::ModifyString::Response> response){
+                    const std::shared_ptr<beginner_tutorials::srv::ModifyString::Response> response) {
     service_message = request->input;
     response->output = request->input;
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request, Change string to: [%s]",request->input.c_str());
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%s]", response->output.c_str());
+    RCLCPP_INFO(this->get_logger(), "Incoming request, Change string to: [%s]", request->input.c_str());
+    RCLCPP_INFO(this->get_logger(), "Sending back response: [%s]", response->output.c_str());
   }
+
+  // Callback to handle frequency parameter updates dynamically
+  void param_callback(const rclcpp::Parameter & param) {
+    RCLCPP_INFO(this->get_logger(), "cb: Received an update to parameter \"%s\" of type %s: %.2f",
+                param.get_name().c_str(), param.get_type_name().c_str(), param.as_double());
+
+    // Update the timer with the new frequency
+    auto period = std::chrono::milliseconds(static_cast<int>(1000 / param.as_double()));
+    timer_->cancel(); // Stop the current timer
+    timer_ = this->create_wall_timer(period, std::bind(&MinimalPublisher::timer_callback, this)); // Restart timer with new rate
+  }
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
   std::string service_message = "Koustubh";
   rclcpp::Service<beginner_tutorials::srv::ModifyString>::SharedPtr service_;
-  std::chrono::milliseconds publisher_rate_;
+  PARAMETER_EVENT m_param_subscriber_;
+  PARAMETER_HANDLE m_param_handle_;
 };
 
 int main(int argc, char* argv[]) {
@@ -60,3 +96,4 @@ int main(int argc, char* argv[]) {
   rclcpp::shutdown();
   return 0;
 }
+
